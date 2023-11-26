9f461dfdc85bdfbaa195fb35d05c24797b7be5aa
mithila.gamage <mithila.gamage@madmobile.com>
2023-11-26 23:49:26 +0530

    feat-tt-03: Adding JS and dummy .JSON file

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const toggleDropdown = (event) => {
     dropdownPanel.classList.toggle('opened');
 };
 
+const selectOption = (event) => {
+    const eventName = event.querySelector('.event-name').textContent;
+    input.value += `${eventName}, `;
+    dropdownPanel.classList.add('opened');
+    updatePlaceholder();
+};
+
 document.addEventListener('click', function (event) {
     const closeDropdownFromOutside = dropdownPanel.contains(event.target);
     if (!closeDropdownFromOutside) {
@@ -20,3 +27,81 @@ dropdown.addEventListener('click', toggleDropdown);
 for (const option of listOfOptions) {
     option.addEventListener('click', selectOption);
 }
+
+async function fetchEvents() {
+    const response = await fetch(`events.json`);
+    const data = await response.json();
+    return data;
+}
+
+async function populateEventsList() {
+    const eventsList = document.getElementById('eventDropdown');
+    const data = await fetchEvents();
+
+    for (const event of data) {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-id', event.id);
+        listItem.innerHTML = `<span class="event-name">${event.name}</span><span class="event-date">${event.date}</span>`
+        listItem.addEventListener('click', toggleSelection);
+        eventsList.appendChild(listItem);
+    };
+
+    const listOfOptions = eventsList.getElementsByTagName('li');
+
+    updatePlaceholder();
+}
+
+function getSelectedEvents() {
+    var selectedEvents = [];
+    var eventsList = document.getElementById('eventDropdown');
+    var selectedItems = eventsList.querySelectorAll('.selected');
+
+    selectedItems.forEach(item => {
+        selectedEvents.push(item.getAttribute('data-id'));
+    });
+
+    return selectedEvents;
+}
+
+function submitForm() {
+    var selectedEvents = getSelectedEvents();
+    console.log('Selected Event IDs: ' + selectedEvents.join(','));
+}
+
+function filterEvents() {
+    var input, filter, eventsList, items, item, i;
+    input = document.getElementById('searchEvent');
+    filter = input.value.toUpperCase();
+    eventsList = document.getElementById('eventDropdown');
+    items = eventsList.getElementsByTagName('li');
+
+    for (i = 0; i < items.length; i++) {
+        item = items[i];
+        const eventText = item.textContent.toUpperCase();
+        if (eventText.indexOf(filter) > -1) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    }
+
+    updatePlaceholder();
+}
+
+function toggleSelection() {
+    this.classList.toggle('selected');
+    dropdownPanel.classList.add('opened');
+
+    updatePlaceholder();
+}
+
+function updatePlaceholder() {
+    var selectedEvents = getSelectedEvents();
+    if (selectedEvents.length > 0) {
+        input.placeholder = selectedEvents.length + ' items selected';
+    } else {
+        input.placeholder = 'Search events';
+    }
+}
+
+populateEventsList();
